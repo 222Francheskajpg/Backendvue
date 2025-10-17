163568d1fd0b147e5ba918887f430e45d4199819
Francheska Martinez <2220014@ipisa.edu.do>
2025-10-17 11:28:24 -0400

    Casi casi

--- db.js
+++ db.js
@@ -1,15 +1,14 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+const SequelizeImport = require('sequelize');
+const Sequelize = SequelizeImport.Sequelize || SequelizeImport.default;
+
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_URL, {
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        },
-    },
-loggin: false,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false },
+  },
+  logging: false,
 });
 
-module.exports = sequelize; 
+module.exports = sequelize;
